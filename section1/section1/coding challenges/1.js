418e3407b3a170164e2403ad62f123f7980a6def
umitkezer <110773926+umitkezer@users.noreply.github.com>
2022-09-14 22:02:16 +0300

    file editing

---
+++ section1/section1/coding challenges/1.js
@@ -0,0 +1,18 @@
+/*
+Mark and john are trying  to comprare their BMI(Body Mass Index)
+which is calculated using the farmula;
+BMI = mass / height ** 2 = mass / (height * height)
+(mass in kg and height in meter)
+
+1.Store Mark's and Jhon's mass and height in veriables.
+2.Calcute both their BMI's using the farmula (you cab even implement both versions)
+3.Create a boolean veriable 'markHigherBMI' containing informatin about whether mark has a higher BMI than Jhon.
+
+
+TEST DATA 1:Marks weights 78 kg and is 1.69 m tall.
+Jhon weights 92 kg and is 1.95 m tall
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall.
+Jhon weights 85 kg and is 1.76 m tall
+
+
+*/
