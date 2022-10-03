8c9a989971e02898148c82283e0bf3d1a98e9945
umitkezer <110773926+umitkezer@users.noreply.github.com>
2022-10-03 13:43:06 +0300

    logical operators

---
+++ logical operators.js
@@ -0,0 +1,21 @@
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if (hasDriversLicense && hasGoodVision) { // hasGoodVision un basina ! koyarsak sonuc if blogu ile degisecektir
+//   console.log("Sarah is able to drive!");
+// } else {
+//   console.log("Someone else should drive...");
+// }
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log("Sarah is able to drive!");
+} else {
+  console.log("Someone else should drive...");
+}
