0fc651eea9284af48e45bc0ed69cbe227dbe7aaf
umitkezer <110773926+umitkezer@users.noreply.github.com>
2022-10-25 13:18:29 +0300

    Update data types.js

---
+++ homework/data types.js
@@ -0,0 +1,11 @@
+const isIsland = false;
+let language;
+const country = "Turkey";
+const continent = "Europe and Asia";
+const population = 84;
+
+console.log(typeof isIsland);
+console.log(typeof language);
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
